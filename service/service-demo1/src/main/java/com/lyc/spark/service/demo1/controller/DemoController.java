7f80b864b1f1f04cd74259ee21bf99cc57bbd660
ethan <306702227@qq.com>
2021-02-03 17:04:31 +0800

    no message

--- service/service-demo1/src/main/java/com/lyc/spark/service/demo1/controller/DemoController.java
+++ service/service-demo1/src/main/java/com/lyc/spark/service/demo1/controller/DemoController.java
@@ -1,2 +1,14 @@
-package com.lyc.spark.service.demo1.controller;public class DemoController {
+package com.lyc.spark.service.demo1.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/liveness")
+    public ResponseEntity liveness() {
+        return ResponseEntity.ok().body("OK----->>");
+    }
 }
